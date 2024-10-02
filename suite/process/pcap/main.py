e5d61e5b3e9d319d65ae71e94e9aed53a67fa1ac
Lorenzo Principi <l.principi8@gmail.com>
2024-10-02 14:47:40 +0200

    suite process pcap

---
+++ suite/process/pcap/main.py
@@ -0,0 +1,41 @@
+
+import argparse
+from dataclasses import dataclass
+import json
+from pathlib import Path
+
+from preprocess import PCAPPreProcess, PCAPBinaries
+
+def process(binaries, input, outdir):
+    ppp = PCAPPreProcess(binaries, input, outdir)
+    df = ppp.run()
+    df.to_csv(outdir.joinpath(input.with_suffix('.csv').name))
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='PCAP Processer',
+                    description='Author: Lorenzo Principi, 2024')
+    
+
+    parser.add_argument('binaries', help='''The .json file where the required binaries path are stored. Required binaries are: tcpdump, tshark, dns_parse.''')
+    parser.add_argument('input', help='The .pcap file to processed.')
+    parser.add_argument('outdir', help='The directory where the output file and the log are saved. If does not exist, it will be created.')
+
+    args = parser.parse_args()
+
+    with open(args.binaries) as fp:
+        binaries_dict = json.load(fp)
+        binaries = PCAPBinaries(
+            dns_parse=binaries_dict['dns_parse'],
+            tcpdump=binaries_dict['tcpdump'],
+            tshark=binaries_dict['tshark']
+        )
+        pass
+    
+    input = Path(args.input)
+    outdir = Path(args.outdir)
+
+    process(binaries, input, outdir)
+
+    pass
---
+++ suite/process/pcap/utils.py
@@ -0,0 +1,69 @@
+
+from pathlib import Path
+import subprocess
+
+
+class Subprocess:
+    INPUT_FLAG = f'%input'
+    OUTPUT_FLAG = f'%output'
+    def __init__(self, name, binary: Path, outdir: Path):
+        self.name = name
+        self.binary = binary
+        self.outdir = outdir.joinpath(self.name)
+        if not self.outdir.exists():
+            self.outdir.mkdir(parents=False, exist_ok=True)
+            pass
+        self.output: Path = self.outdir.joinpath(f'{self.name}.output')
+        pass
+
+    def is_done(self):
+        if self.output is not None and self.output.exists():
+            print(f"[info]: skipping {self.output.name}, file already exists.")
+            return True
+        return False
+
+    def log(self):
+        return self.outdir.joinpath(f'{self.name}.log')
+
+    def rename_file_if_error(self, is_test_failed: bool):
+        if is_test_failed and self.output is not None and self.output.exists():
+            self.output.rename(f"{self.output}.error")
+        pass
+    
+    def launch(self, input: Path, args):
+        if self.is_done():
+            return
+        
+        args = [ input if arg == Subprocess.INPUT_FLAG else arg for arg in args ]
+        args = [ self.output if arg == Subprocess.OUTPUT_FLAG else arg for arg in args ]
+        args = [ arg.absolute() if isinstance(arg, Path) else arg for arg in args ]
+        args = [self.binary] + args
+
+        print(f"[info]: {self.name} step...")
+        with open(self.log(), "w") as fplog:
+            try:
+                p = subprocess.Popen(
+                    args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True
+                )
+            except Exception as e:
+                print(e, "\n\n".join([ str(arg) for arg in args ]))
+                exit(1)
+                pass
+
+            output, errors = p.communicate(timeout=100000)
+
+            fplog.writelines(output)
+            fplog.writelines(errors)
+
+            self.rename_file_if_error(p.returncode != 0)
+
+            if p.returncode != 0:
+                print(output)
+                print(errors)
+                raise Exception(f"{self.name} execution failed")
+            pass
+        pass # method launch
+    pass # class Subprocess
